a5d188d4657f73090861cf8642e475427a6c953d
yue4u <github@yue.coffee>
2022-06-12 16:41:24 +0900

    feat: timer

--- scripts/level.gd
+++ scripts/level.gd
@@ -7,12 +7,22 @@ onready var goals = $Goals.get_children()
 onready var goals_label = $UI/MarginContainer/HBoxContainer/HBoxContainer/Goals
 onready var all = goals.size();
 
+onready var time_label = $UI/MarginContainer/HBoxContainer/Time;
+onready var timer = $UI/MarginContainer/HBoxContainer/Time/Timer
+
 var clear = false
+var time = 0
 
 func _ready():
 	print("%s is ready" % scene_name)
-	get_tree().paused = false
 	level_label.text = scene_name
+
+	get_tree().paused = false
+
+	timer.connect("timeout", self, "_every_sec")
+	timer.set_wait_time(1.0)
+	timer.set_one_shot(false) # Make sure it loops
+	timer.start()
 
 func _process(_delta):
 	# give me reduce...
@@ -33,4 +43,10 @@ func load_next():
 	yield(get_tree().create_timer(1.0), "timeout")
 	var next_level = int(scene_name) + 1
 	var __ = get_tree().change_scene("res://levels/level_%s.tscn" % next_level)
+
+func _every_sec():
+	time +=1
+	var _min = int(time / 60)
+	var _sec = time % 60
+	time_label.text = "%02d : %02d" % [_min, _sec]
 
